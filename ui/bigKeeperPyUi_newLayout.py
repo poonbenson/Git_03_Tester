6199fce468c255c26e6a60ce5696fad587f3ab17
poonbenson <56299311+poonbenson@users.noreply.github.com>
2024-08-16 22:58:35 +0100

    20240816
    this is update is for lightPublish:
    
    - Add button for create Backdrop for LightPublish
    - Add button for execute VersionUp + LightPublish
    
    Future
    ======
    Need to fix the problem for light images, that same render layer folder have more than one exr sequence.

--- ui/bigKeeperPyUi_newLayout.py
+++ ui/bigKeeperPyUi_newLayout.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'bigKeeperPyUi_newLayout_v136.ui'
+## Form generated from reading UI file 'bigKeeperPyUi_newLayout_v138.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.15.2
 ##
@@ -225,6 +225,15 @@ class Ui_MainWindow(object):
         self.pushButton_genWriteCompMasterV = QPushButton(self.tabNuke)
         self.pushButton_genWriteCompMasterV.setObjectName(u"pushButton_genWriteCompMasterV")
         self.pushButton_genWriteCompMasterV.setGeometry(QRect(130, 150, 91, 23))
+        self.label_23 = QLabel(self.tabNuke)
+        self.label_23.setObjectName(u"label_23")
+        self.label_23.setGeometry(QRect(0, 20, 81, 16))
+        self.pushButton_genLightPublishBackdrop = QPushButton(self.tabNuke)
+        self.pushButton_genLightPublishBackdrop.setObjectName(u"pushButton_genLightPublishBackdrop")
+        self.pushButton_genLightPublishBackdrop.setGeometry(QRect(0, 40, 121, 41))
+        self.pushButton_LightPublishBackdrop = QPushButton(self.tabNuke)
+        self.pushButton_LightPublishBackdrop.setObjectName(u"pushButton_LightPublishBackdrop")
+        self.pushButton_LightPublishBackdrop.setGeometry(QRect(130, 40, 91, 41))
         self.tabWidget_2.addTab(self.tabNuke, "")
         self.tabHoudini = QWidget()
         self.tabHoudini.setObjectName(u"tabHoudini")
@@ -846,8 +855,8 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
 
-        self.tabWidget.setCurrentIndex(1)
-        self.tabWidget_2.setCurrentIndex(5)
+        self.tabWidget.setCurrentIndex(0)
+        self.tabWidget_2.setCurrentIndex(1)
 
 
         QMetaObject.connectSlotsByName(MainWindow)
@@ -919,6 +928,17 @@ class Ui_MainWindow(object):
         self.pushButton_genWriteCompMasterV.setToolTip(QCoreApplication.translate("MainWindow", u"pushButton_genWriteCompMaster", None))
 #endif // QT_CONFIG(tooltip)
         self.pushButton_genWriteCompMasterV.setText(QCoreApplication.translate("MainWindow", u"CompMaster-V", None))
+        self.label_23.setText(QCoreApplication.translate("MainWindow", u"Light Publish :", None))
+#if QT_CONFIG(tooltip)
+        self.pushButton_genLightPublishBackdrop.setToolTip(QCoreApplication.translate("MainWindow", u"pushButton_genWritePrerend", None))
+#endif // QT_CONFIG(tooltip)
+        self.pushButton_genLightPublishBackdrop.setText(QCoreApplication.translate("MainWindow", u"Backdrop (only 1)\n"
+"(for LightPublish)", None))
+#if QT_CONFIG(tooltip)
+        self.pushButton_LightPublishBackdrop.setToolTip(QCoreApplication.translate("MainWindow", u"pushButton_genWritePrerend", None))
+#endif // QT_CONFIG(tooltip)
+        self.pushButton_LightPublishBackdrop.setText(QCoreApplication.translate("MainWindow", u"VersionUp + \n"
+"LightPublish", None))
         self.tabWidget_2.setTabText(self.tabWidget_2.indexOf(self.tabNuke), QCoreApplication.translate("MainWindow", u"Nuke", None))
         self.tabWidget_2.setTabText(self.tabWidget_2.indexOf(self.tabHoudini), QCoreApplication.translate("MainWindow", u"Houdini", None))
         self.tabWidget_2.setTabText(self.tabWidget_2.indexOf(self.tabBlender), QCoreApplication.translate("MainWindow", u"Blender", None))
